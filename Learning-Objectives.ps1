b2d642a1e8a8ca472ae3c0d0bff1c0b9bcf3028c
Rektsu <fonsobr@gmail.com>
2022-03-10 19:10:07 +0100

    Update Learning-Objectives.ps1

--- Learning-Objectives.ps1
+++ Learning-Objectives.ps1
@@ -13,3 +13,19 @@
   
 - Enterprise Administrators
   Get-DomainGroupMember -domain moneycorp.local -Identity "Enterprise Admins" -Recurse | select MemberName
+
+# Learning Objective 2
+
+- List all the OUs
+  Get-DomainOU | select name
+
+- List all the computers in the StudentMachines OU
+  (Get-DomainOU -Identity StudentMachines).distinguishedname | %{Get-DomainComputer -SearchBase $_} | select name
+  
+- List the GPOs
+  Get-DomainGPO | select -ExpandProperty displayname
+  
+- Enumerate GPO applied on the StudentMachines OU
+  (Get-DomainOU -Identity StudentMachines).gplink
+  Get-DomainGPO -Identity "{3E04167E-C2B6-4A9A-8FB7-C811158DC97C}"
+  
