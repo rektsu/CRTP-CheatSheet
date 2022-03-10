300d139d8a86f6ae581f795f42be70b93bf94178
Rektsu <fonsobr@gmail.com>
2022-03-10 19:41:33 +0100

    Create Forest-Enum.ps1

---
+++ Forest-Enum.ps1
@@ -0,0 +1,11 @@
+# FOREST MAPPING
+Get-Forest -Forest <forest>
+
+# DOMAINS IN FOREST
+Get-ForestDomain -Forest <forest>
+
+# GLOBAL CATALOGS FOR FOREST
+Get-ForestGlobalCatalog -Forest <forest>
+
+# TRUSTS
+Get-ForestTrust -Forest <forest>
